e770b4cdc93f534c52799d7538da10ce8752dc05
Chris Barclay <chris.barclay1@gmail.com>
2023-05-17 14:40:30 +0100

    Lessons can be added and deleted

--- src/main/java/com/codeclan/classroombooking/components/DataLoader.java
+++ src/main/java/com/codeclan/classroombooking/components/DataLoader.java
@@ -35,13 +35,13 @@ public class DataLoader implements ApplicationRunner {
         userRepository.save(user);
         userRepository.save(user2);
         userRepository.save(user3);
-        Lesson lesson = new Lesson("Art & Design", DayType.MON, 3, 1, user2);
         Lesson lesson2 = new Lesson("History", DayType.MON, 1, 1, user2);
         Lesson lesson3 = new Lesson("Photography", DayType.MON, 2, 1, user2);
+        Lesson lesson = new Lesson("Art & Design", DayType.MON, 3, 1, user2);
         Lesson lesson4 = new Lesson("Photography", DayType.WED, 1, 1, user2);
-        lessonRepository.save(lesson);
         lessonRepository.save(lesson2);
         lessonRepository.save(lesson3);
+        lessonRepository.save(lesson);
         lessonRepository.save(lesson4);
         Student student = new Student("Dale", "Cooper", 1);
         Student student2 = new Student("Laura", "Palmer", 1);
